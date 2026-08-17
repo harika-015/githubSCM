bbb306d563aae67c31f8ad681c01d6e5e5c85f5b
harika-015 <245123749015@mvsrec.edu.in>
2026-08-17 13:32:40 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -2,5 +2,6 @@ public class demo1{
   public static void main(String args[]){
     System.out.println("This is java code");
     System.out.println("Namaste java");
+    System.out.println("Hello");
   }
 }
